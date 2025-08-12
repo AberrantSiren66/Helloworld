34822a103870537c0d29c6f3115566dc33f6214f
AberrantSiren66 <marlonswart830@gmail.com>
2025-08-12 22:24:02 +0200

    miracle

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
+from datetime import date
+
 def say_hello(name):
     print("hello, "+ name)
 
 say_hello("VS Code")
 
-def say_day_of_week(date): 
-    print("Today is " + date)
+def say_day_of_week(date_str):
+    print("Today is " + date_str)
 
-    say_day_of_week(date.today())
-    
+def call_with_today(say_day_of_week):
+    say_day_of_week(date.today().isoformat())
+
+call_with_today(say_day_of_week)
